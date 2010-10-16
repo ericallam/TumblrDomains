d1477a1af6562633cad8f12d24fc0b956a75ad96
Eric Allam <rubymaverick@gmail.com>
2010-10-16 10:41:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumblrdomains::Application.config.secret_token = '75356db4b771f47ba69dd6305e0e018d7085e14e545d7c1b55e9ab1aea097af995220afdba6fb355ab7fe669741b695cdb916818e9e5f11b32e7ef48d46a1cec'
---
+++ lib/domain_changer.rb
@@ -0,0 +1,30 @@
+module DomainChanger
+  mattr_accessor :handler
+
+  class TumblrChanger
+    def initialize(blog_name, email, password)
+      @blog_name = blog_name
+      @email = email
+      @password = password
+    end
+
+    def change_to(domain_name)
+      agent = Mechanize.new
+
+      login_page = agent.get('http://www.tumblr.com/login')
+      login_form = login_page.form_with(:action => "/login")
+      login_form.email = @email
+      login_form.password = @password
+
+      agent.submit(login_form)
+
+      customize_page = agent.get("http://www.tumblr.com/customize/#{@blog_name}")
+      customize_form = c.form_with(:action => "http://www.tumblr.com/customize/#{@blog_name}")
+      customize_form.checkbox_with(:name => 'enable_cname').check
+      customize_form.cname = domain_name
+
+      agent.submit(customize_form)
+    end
+
+  end
+end
